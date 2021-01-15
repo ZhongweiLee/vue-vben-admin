e75b7ab434151b76679c4ecd652a16fcaf588f87
lizhongwei <zhongwei-li@qq.com>
2021-01-15 15:19:29 +0800

    feat: dict

--- src/views/system/dict/DictType.vue
+++ src/views/system/dict/DictType.vue
@@ -1,29 +1,38 @@
 <template>
-  <BasicTable @register="registerTable">
-    <template #action="{ record }">
-      <TableAction
-        :actions="[
-          {
-            label: '删除',
-            icon: 'ic:outline-delete',
-            onClick: handleDelete.bind(null, record),
-          },
-        ]"
-        :outside="true"
-      />
-    </template>
-  </BasicTable>
+  <PageWrapper>
+    <BasicTable @register="registerTable">
+      <template #action="{ record }">
+        <TableAction
+          :actions="[
+            {
+              label: '删除',
+              icon: 'ic:outline-delete',
+              onClick: handleDelete.bind(null, record),
+            },
+          ]"
+        />
+      </template>
+      <template #toolbar>
+        <a-button type="primary" @click="handleAdd"> 添加字典 </a-button>
+      </template>
+    </BasicTable>
+    <TypeAdd @register="registerAdd" />
+  </PageWrapper>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import { useModal } from '/@/components/Modal';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
-  import { getDictTypeColumns, getDictTypeFormConfig } from './dictTypeData';
-
-  import { dictTypeDeleteApi, dictTypeListApi } from '/@/api/system/dict/dict';
+  import { PageWrapper } from '/@/components/Page';
   import { useMessage } from '/@/hooks/web/useMessage';
 
+  import { getDictTypeColumns, getDictTypeFormConfig } from './dictTypeData';
+  import { dictTypeDeleteApi, dictTypeListApi } from '/@/api/system/dict/dict';
+
+  import TypeAdd from './TypeAdd.vue';
+
   export default defineComponent({
-    components: { BasicTable, TableAction },
+    components: { BasicTable, TableAction, PageWrapper, TypeAdd },
     setup() {
       const [registerTable, { reload }] = useTable({
         title: '字典列表',
@@ -50,9 +59,18 @@
         } catch (error) {}
       }
 
+      const [registerAdd, { openModal: openModalAdd }] = useModal();
+
+      function handleAdd() {
+        console.log('click add....');
+        openModalAdd(true, {}, true);
+      }
+
       return {
         registerTable,
         handleDelete,
+        handleAdd,
+        registerAdd,
       };
     },
   });
